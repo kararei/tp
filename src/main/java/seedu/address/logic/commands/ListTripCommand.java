386997457d9a2db6551779c306ccb7003539e86a
kararei <kararei@gmail.com>
2025-03-20 19:05:04 +0800

    Edit Message Success literal

--- src/main/java/seedu/address/logic/commands/ListTripCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTripCommand.java
@@ -2,8 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TRIPS;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_SERVICE;
 
 import seedu.address.model.Model;
 import seedu.address.model.trip.Trip;
@@ -21,7 +19,7 @@ public class ListTripCommand extends Command {
             + "Examples: "
             + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Listed all %s trips";
+    public static final String MESSAGE_SUCCESS = "Listed all trips";
 
     @Override
     public CommandResult execute(Model model) {
