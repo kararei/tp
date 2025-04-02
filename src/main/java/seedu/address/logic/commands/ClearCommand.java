4c78be23f0dfcdaa9f98a455b753d126dc8bc7e5
SuspectBlue <jovanyosh17@gmail.com>
2025-04-02 11:21:18 +0800

    Add clear trips functionality to clear command

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -4,20 +4,22 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.TripBook;
 
 /**
- * Clears the address book.
+ * Clears the address book and trip book.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "Address book and Trip book has been cleared!";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setAddressBook(new AddressBook());
+        model.setTripBook(new TripBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
