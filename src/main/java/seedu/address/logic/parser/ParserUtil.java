dd818bcb7af58f27b079e70de7a612be11ba0d4d
SuspectBlue <jovanyosh17@gmail.com>
2025-04-08 13:40:11 +0800

    Update Javadocs

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -31,7 +31,9 @@ public class ParserUtil {
     private static final String SPECIAL_CHARACTERS = "!#$%&'*+/=?`{|}~^.-";
 
     /**
-     * @throws ParseException if the specified index is invalid (not non-zero unsigned integer).
+     * Parses {@code oneBasedIndex} for a contact into an {@code Index} and returns it. Leading and trailing
+     * whitespaces will be trimmed.
+     * @throws ParseException if the specified index is invalid (not a non-zero unsigned integer).
      */
     public static Index parseContactIndex(String oneBasedIndex) throws ParseException {
         String trimmedIndex = oneBasedIndex.trim();
